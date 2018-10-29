8933545f3674cc443344480e7843ee1f18279d15
Dalton Lima <daltonvarussa@gmail.com>
2018-10-29 18:52:30 -0300

    add main and test

---
+++ Fractals/Fractals.cpp
@@ -0,0 +1,21 @@
+#include "ecrireimage.h"
+#include <iostream>
+
+/* Headers */
+void test();
+
+int main()
+{	
+	std::cout << "Fractales par IFS" << std::endl;
+
+	test();	
+
+	std::cin.get();
+}
+
+/* Function that calls all other tests */
+void test()
+{
+	std::cout << "Starting tests..." << std::endl;
+	// call the desired tests
+}
